507e38ef44aba3915ed30110b45d25500db369a3
Vivvianne <ndiahav@gmail.com>
2020-03-11 12:00:47 +0300

    added the trends and who tollow parts

--- src/components/trends/TrendsComponent.vue
+++ src/components/trends/TrendsComponent.vue
@@ -1,27 +1,107 @@
 <template lang="pug">
   div()
-      v-form
-        v-text-field.form1(placeholder='Search Twitter', filled rounded, dense)
-      v-card.background()
-        v-card-text.card
-                v-list-item-title.h1 Trends for you
+    //- Search Bar
+    v-text-field.form1(prepend-inner-icon="search" placeholder='Search Twitter', filled, rounded, dense)
+
+
+    .trends.pa-5()
+      .d-flex.justify-space-between()
+        .title() Trends for you
+        v-icon( color="primary") settings
+
+    
+      v-list.trendsBackground(two-line)
+        v-list-item(v-for="(item,index) in trendList" :key="index" @click="")
+          v-list-item-content()
+            v-list-item-title()
+              .d-flex.justify-space-between()
+                | {{item.name}}
+                v-icon(small color="black") expand_more
+            v-list-item-subtitle.text--grey() {{item.tweets}} Tweets
+
+      .caption.primary--text(v-if="trendList.length > 3") Show More
+
+    .wtfollow.pa-5()
+      .d-flex.justify-space-between()
+        .title() Who to follow
         
+
+    
+      v-list.tfollowBackground(two-line)
+        v-list-item(v-for="(user,index) in tfollowList" :key="index" @click="")
+          v-list-item-content()
+            v-list-item-title()
+              .d-flex.justify-space-between()
+                v-avatar.img
+                      img(src='https://cdn.vuetifyjs.com/images/john.jpg', alt='me')
+                div.mx-2()
+                  | {{user.user}}
+                  v-list-item-subtitle.text--grey() {{user.handle}} 
+                v-btn(rounded='', color='primary' outlined small) follow
+            
+
+      .caption.primary--text() Show More
 
 
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    trendList: [
+      {
+        name: "#CoronaVirusUpdate",
+        tweets: 1456,
+        link: null
+      },
+      {
+        name: "#Joe Biden",
+        tweets: 304,
+        link: null
+      },
+      {
+        name: "#Premier League",
+        tweets: 52,
+        link: null
+      }
+    ],
+    tfollowList: [
+      {
+        user: "#Migunamiguna",
+        handle: "@migunamiguna",
+        link: null
+      },
+      {
+        user: "#Fiji Rugby Union",
+        handle: "@fijirugby",
+        link: null
+      },
+
+      {
+        user: "#USA Rugby Union",
+        handle: "@usarugby",
+        link: null
+      },
+
+    ]
+
+  })
+};
 </script>
 
 <style>
-.form1{
-  width: 325px;
+.form1 {
+  /* width: 325px; */
 }
-.card{
-  background-color:#E2EAF8;
+.trends {
   color: #000000;
   font-weight: bold;
   font-size: 15px;
+  width: 100%;
+  height: 400px;
+  border-radius: 15px;
+}
+.trendsBackground {
+  background-color: #e2eaf8;
 }
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,20 @@
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-
+import colors from "vuetify/lib/util/colors";
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    iconfont: "md" // default - only for display purposes
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: "#1DA1F2", // #E53935
+        secondary: colors.red.lighten4, // #FFCDD2
+        accent: colors.orange // #3F51B5
+      }
+    }
+  }
+});
