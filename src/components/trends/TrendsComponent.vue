fb97b73a85dfd36c57c22307824e4ed63b2f4861
Vivvianne <ndiahav@gmail.com>
2020-03-11 09:55:32 +0300

    added the search part

--- src/components/trends/TrendsComponent.vue
+++ src/components/trends/TrendsComponent.vue
@@ -1,9 +1,27 @@
 <template lang="pug">
-  div() Here is my trends
+  div()
+      v-form
+        v-text-field.form1(placeholder='Search Twitter', filled rounded, dense)
+      v-card.background()
+        v-card-text.card
+                v-list-item-title.h1 Trends for you
+        
+
+
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.form1{
+  width: 325px;
+}
+.card{
+  background-color:#E2EAF8;
+  color: #000000;
+  font-weight: bold;
+  font-size: 15px;
+}
+</style>
